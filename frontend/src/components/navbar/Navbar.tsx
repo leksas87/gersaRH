20371778c87364e4014ba19c7166c6209d3c80c8
Ivan <ivan.santana@ulfix.com>
2022-01-06 21:32:03 -0600

    sidebar y navbar - listo

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -4,9 +4,18 @@ const Navbar = () => {
 		<>
 			<div className='d-flex '>
 				<nav className='d-flex align-items-center justify-content-between navbarContainer'>
-					<div>btn</div>
-					<div>Logo</div>
-					<div>icon</div>
+					<div className='navbarBtnMenu d-flex justify-content-center'>
+						<i className='bi bi-list navbarIcon' />
+					</div>
+					<div>
+						<img width='100px' src='assets\gersa-logo.png' alt='gersa-logo' />
+					</div>
+					<div>
+						<i
+							className='bi bi-person-circle navbarIcon'
+							style={{ color: '#ff0062' }}
+						/>
+					</div>
 				</nav>
 			</div>
 		</>
