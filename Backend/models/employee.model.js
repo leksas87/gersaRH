2b8ee8db5d0f36772a97fbe04709147aa27259b8
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-01-26 13:50:55 -0600

    commit datos excel a modelo

--- Backend/models/employee.model.js
+++ Backend/models/employee.model.js
@@ -14,6 +14,14 @@ function model(sequelize) {
         supervisor: { type: DataTypes.STRING, allowNull:false },
         numeroCuentaBancaria:{type: DataTypes.INTEGER,allowNull:true},
         frecuenciaPago:{ type: DataTypes.STRING, allowNull:false },
+        direccion1:{ type:DataTypes.STRING,allowNull:false },
+        direccion2:{ type:DataTypes.STRING,allowNull:true },
+        ciudad:{type:DataTypes.STRING,allowNull:false},
+        codigoPostal:{type:DataTypes.INTEGER,allowNull:false},
+        estadoProvincia:{type:DataTypes.STRING,allowNull:false},
+        pais:{type:DataTypes.STRING,allowNull:false},
+        emergenciaNombre:{type:DataTypes.STRING,allowNull:true},
+        empergenciaTelefono:{type:DataTypes.STRING,allowNull:true},
     };
 
 
