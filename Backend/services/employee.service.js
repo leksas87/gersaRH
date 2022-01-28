5326062de5911b72d70dbe85a297b316bbe3b064
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-01-27 19:21:44 -0600

    primer comit

--- Backend/services/employee.service.js
+++ Backend/services/employee.service.js
@@ -4,93 +4,13 @@ const bcrypt = require('bcryptjs');
 const {models} = require('./../libs/sequelize');
 
 module.exports = {
-    authenticate,
-    getAll,
-    getById,
+   
     create,
-    createMaster,
-    update,
-    delete: _delete,
-    reenvioToken,
-    getByToken,
-    updateConfirmation,
+    
 };
 
-async function authenticate({ username, password }) {
-    const user = await models.User.findOne({ where: { username } });
-
-    if (user.active != true)       
-          throw "Cuenta inactiva, favor de verificar su email";
-
-
-    if (!user || !(await bcrypt.compare(password, user.hash)))
-        throw 'Usuario o contraseña incorrecta';
-
-    // authentication successful
-    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '2h' });
-    return { ...omitHash(user.get()), token };
-}
-
-async function reenvioToken({ username, password }) {
-    const user = await models.User.scope('withHash').findOne({ where: { username } });
-
-    if (!user )
-        throw 'Usuario o contraseña incorrecta';
-
-    // authentication successful
-    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '2h' });
-    return { ...omitHash(user.get()), token };
-}
-
-async function getAll() {
-    return await models.User.findAll();
-}
-
-async function getById(id) {
-    return await getUser(id);
-}
-
-async function getByToken(token) {
-    return await getUserToken(token);
-}
-
 async function create(params) {
-    // validate
-    if (await models.User.findOne({ where: { username: params.username } })) {
-        throw 'El Usuario "' + params.username + '" ya existe en el sistema';
-    }
-
-    const token = jwt.sign({email: params.username}, config.secret);
-
-    params.confirmationCode=token;
-
-    const sgMail = require('@sendgrid/mail');
-
-    const API_KEY=process.env.SENDGRID_API_KEY
-
-    const URL=process.env.URL
-    
-    try {
-        sgMail.setApiKey(API_KEY)
-        const url=URL+params.confirmationCode;
-        console.log(url);
-        const msg = {
-            to: params.username,
-            from: {email:process.env.EMAIL,name:process.env.NAME,},
-            subject:'Confirmación de registro',
-            templateId: process.env.TEMPLETE,
-            dynamic_template_data: {
-                url: url,
-            },
-  
-        };
-        await sgMail.send(msg);
-        // save user
-        await models.User.create(params);
-    } catch (error) {
-        console.log(error.message);
-    }
-    
+     await models.Employee.create(params);
 }
 
 async function createMaster(params) {
