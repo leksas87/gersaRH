9e8a1386c06383ea7ea68d14aa0df54d026470e3
Ivan <ivan.santana@ulfix.com>
2022-01-28 02:44:03 -0600

    contactoEmergencia, infoBancaria, Num seguro social - Listo

--- frontend/src/components/empleados/empleadoPerfil/PageEmpleadoPersonal.tsx
+++ frontend/src/components/empleados/empleadoPerfil/PageEmpleadoPersonal.tsx
@@ -10,6 +10,9 @@ const PageEmpleadoPersonal = () => {
 	const [value, toggleValue] = useToggle(false); //Recibe el valor inicial
 	const [ddiValue, toggleDdiValue] = useToggle(false); //Recibe el valor inicial
 	const [direccionValue, toggleDireccion] = useToggle(false); //Recibe el valor inicial
+	const [contactoEValue, toggleContactoE] = useToggle(false); //Recibe el valor inicial
+	const [infoBankEValue, toggleInfoBank] = useToggle(false); //Recibe el valor inicial
+	const [numSafeEValue, toggleNumSafe] = useToggle(false); //Recibe el valor inicial
 
 	//Submit del formulario
 	const handlesubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -292,6 +295,231 @@ const PageEmpleadoPersonal = () => {
 									style={{ height: '3rem' }}
 								>
 									{direccionValue && (
+										<button type='submit' className='btn  custm-empleadoFormSubmit'>
+											Guardar
+										</button>
+									)}
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				{/* Contacto de Emergencia */}
+				<div className='d-flex flex-wrap custm-Width100 custm-UnderLineSection'>
+					{/* Izquierda */}
+					<div className='custm-empleadoContainerContent'>
+						<div className='d-flex mt-3 flex-column align-items-start ps-1 custm-Width100'>
+							<div className='custm-Width100 d-flex align-items-center'>
+								<i
+									style={{ fontSize: '3rem', color: 'var(--textColorLink)' }}
+									className='bi bi-person'
+								/>
+								<i
+									style={{
+										fontSize: '1.5rem',
+										color: 'var(--textColorLink)',
+										marginLeft: '-5px',
+									}}
+									className='bi bi-heart'
+								/>
+							</div>
+							<div className='fs-4'>Contacto de emergencia</div>
+							<div className='fs-5 textColorLight'>
+								Completa la información del contacto de emergencia.
+							</div>
+							<div className='custm-underLineTittle'></div>
+						</div>
+					</div>
+					{/* Derecha */}
+					<div className='custm-empleadoContainerContent'>
+						<div className='d-flex flex-column align-items-center custm-empleadoFormContainer'>
+							<div className='d-flex justify-content-end custm-Width100'>
+								<button className='btn fs-3 custm-btnToggle' onClick={toggleContactoE}>
+									<i className='bi bi-pencil-square textColorSecondary' />
+								</button>
+							</div>
+							{/* Inicia formulario */}
+							<form style={{ width: '90%' }} onSubmit={handlesubmit}>
+								<div className='mb-4'>
+									<label className='custm-Width100'>Nombre</label>
+									<input
+										className='form-control custm-Width100 custm-empleadoFormIntput'
+										type='text'
+										placeholder='Nombre'
+										disabled={!contactoEValue}
+									/>
+								</div>
+								<div className='mb-4'>
+									<label className='custm-Width100'>Teléfono</label>
+
+									<input
+										className='form-control custm-Width100 custm-empleadoFormIntput'
+										type='text'
+										placeholder='Teléfono'
+										disabled={!contactoEValue}
+									/>
+								</div>
+
+								<div
+									className='d-flex justify-content-end custm-Width100'
+									style={{ height: '3rem' }}
+								>
+									{contactoEValue && (
+										<button type='submit' className='btn  custm-empleadoFormSubmit'>
+											Guardar
+										</button>
+									)}
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				{/* Información bancaria */}
+				<div className='d-flex flex-wrap custm-Width100 custm-UnderLineSection'>
+					{/* Izquierda */}
+					<div className='custm-empleadoContainerContent'>
+						<div className='d-flex mt-3 flex-column align-items-start ps-1 custm-Width100'>
+							<div className='custm-Width100 d-flex align-items-end'>
+								<i
+									style={{ fontSize: '3rem', color: 'var(--textColorLink)' }}
+									className='bi bi-bank2'
+								/>
+								<i
+									style={{
+										fontSize: '1.5rem',
+										color: 'var(--textColorLink)',
+									}}
+									className='bi bi-cash-coin'
+								/>
+							</div>
+							<div className='fs-4'>Información bancaria</div>
+							<div className='fs-5 textColorLight'>
+								Información sobre la cuenta bancaria del empleado
+							</div>
+							<div className='custm-underLineTittle'></div>
+						</div>
+					</div>
+					{/* Derecha */}
+					<div className='custm-empleadoContainerContent'>
+						<div className='d-flex flex-column align-items-center custm-empleadoFormContainer'>
+							<div className='d-flex justify-content-end custm-Width100'>
+								<button className='btn fs-3 custm-btnToggle' onClick={toggleInfoBank}>
+									<i className='bi bi-pencil-square textColorSecondary' />
+								</button>
+							</div>
+							{/* Inicia formulario */}
+							<form style={{ width: '90%' }} onSubmit={handlesubmit}>
+								<div className='mb-4'>
+									<label className='custm-Width100'>Cuenta bancaria</label>
+									<input
+										className='form-control custm-Width100 custm-empleadoFormIntput'
+										type='text'
+										placeholder='Cuenta bancaria'
+										disabled={!infoBankEValue}
+									/>
+								</div>
+								<div className='mb-4'>
+									<label className='custm-Width100'>SWIFT/BIC</label>
+
+									<input
+										className='form-control custm-Width100 custm-empleadoFormIntput'
+										type='text'
+										placeholder='SWIFT/BIC'
+										disabled={!infoBankEValue}
+									/>
+								</div>
+
+								<div
+									className='d-flex justify-content-end custm-Width100'
+									style={{ height: '3rem' }}
+								>
+									{infoBankEValue && (
+										<button type='submit' className='btn  custm-empleadoFormSubmit'>
+											Guardar
+										</button>
+									)}
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				{/* Número de la Seguridad Social */}
+				<div className='d-flex flex-wrap custm-Width100 custm-UnderLineSection'>
+					{/* Izquierda */}
+					<div className='custm-empleadoContainerContent'>
+						<div className='d-flex mt-3 flex-column align-items-start ps-1 custm-Width100'>
+							<div className='custm-Width100 d-flex align-items-end'>
+								<i
+									style={{ fontSize: '3rem', color: 'var(--textColorLink)' }}
+									className='bi bi-bank'
+								/>
+								<i
+									style={{
+										fontSize: '1.4rem',
+										color: 'var(--textColorLink)',
+										marginLeft: '5px',
+									}}
+									className='bi bi-plus-square-fill'
+								/>
+							</div>
+							<div className='fs-4'>Número de la Seguridad Social</div>
+							<div className='fs-5 textColorLight'>
+								Número de la Seguridad Social del empleado
+							</div>
+							<div className='custm-underLineTittle'></div>
+						</div>
+					</div>
+					{/* Derecha */}
+					<div className='custm-empleadoContainerContent'>
+						<div className='d-flex flex-column align-items-center custm-empleadoFormContainer'>
+							<div className='d-flex justify-content-end custm-Width100'>
+								<button className='btn fs-3 custm-btnToggle' onClick={toggleNumSafe}>
+									<i className='bi bi-pencil-square textColorSecondary' />
+								</button>
+							</div>
+							{/* Inicia formulario */}
+							<form style={{ width: '90%' }} onSubmit={handlesubmit}>
+								<div className='mb-4'>
+									<label className='custm-Width100'>RFC</label>
+									<input
+										className='form-control custm-Width100 custm-empleadoFormIntput'
+										type='text'
+										placeholder='RFC'
+										disabled={!numSafeEValue}
+									/>
+								</div>
+								<div className='mb-4'>
+									<label className='custm-Width100'>Número IMSS</label>
+									<input
+										className='form-control custm-Width100 custm-empleadoFormIntput'
+										type='text'
+										placeholder='Número IMSS'
+										disabled={!numSafeEValue}
+									/>
+								</div>
+								<div className='mb-4'>
+									<label className='custm-Width100'>CURP</label>
+									<input
+										className='form-control custm-Width100 custm-empleadoFormIntput'
+										type='text'
+										placeholder='CURP'
+										disabled={!numSafeEValue}
+									/>
+								</div>
+								<div className='mb-4'>
+									<label className='custm-Width100'>Fecha alta IMSS</label>
+									<input
+										className='form-control custm-Width100 custm-empleadoFormIntput'
+										type='text'
+										placeholder='Fecha alta IMSS'
+										disabled={!numSafeEValue}
+									/>
+								</div>
+								<div
+									className='d-flex justify-content-end custm-Width100'
+									style={{ height: '3rem' }}
+								>
+									{numSafeEValue && (
 										<button type='submit' className='btn  custm-empleadoFormSubmit'>
 											Guardar
 										</button>
