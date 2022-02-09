6cc2b5825e37f5ad4f0aab76e05ba68eee374b8b
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-02-09 13:05:41 -0600

    commit se realiza el reset del envio a hash

--- Backend/services/user.service.js
+++ Backend/services/user.service.js
@@ -234,7 +234,7 @@ async function _delete(id) {
 async function getUser(id) {
     const user = await models.User.findByPk(id);
     if (!user) throw 'Usuario no encontrado';
-    return omitHash(user.get());
+    return user;
 }
 
 async function getUserToken(token) {
