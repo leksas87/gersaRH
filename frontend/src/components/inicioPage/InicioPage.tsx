3305579683c561a20937990adb2d61c92d08b0a0
Ivan <ivan.santana@ulfix.com>
2022-02-11 02:48:06 -0600

    diseño responsive del grid - Listo

--- frontend/src/components/inicioPage/InicioPage.tsx
+++ frontend/src/components/inicioPage/InicioPage.tsx
@@ -8,8 +8,30 @@ const InicioPage = () => {
 	return (
 		<>
 			<div className='custm-inicioContainer'>
-				<div className='d-flex flex-wrap custm-flexContainer'>
-					<div className='custm-iPost d-flex justify-content-center align-items-center flex-column shadow'>
+				<div className=' custm-ContainerListGrid'>
+					<div className='custm-grid-area grid-area-post1'>
+						<div className='custm-iPost1 shadow-sm'>Bien venido</div>
+					</div>
+					<div className='custm-grid-area grid-area-post2'>
+						<div className='custm-iPost2 shadow-sm'>Tareas pendientes</div>
+					</div>
+					<div className='custm-grid-area grid-area-post3'>
+						<div className='custm-iPost shadow-sm'>Avisos</div>
+					</div>
+					<div className='custm-grid-area grid-area-post4'>
+						<div className='custm-iPost shadow-sm'>Proximas ausencias</div>
+					</div>
+					<div className='custm-grid-area grid-area-post5'>
+						<div className='custm-iPost shadow-sm'>Proximos eventos</div>
+					</div>
+					<div className='custm-grid-area grid-area-post6'>
+						<div className='custm-iPost shadow-sm'>Proximos feriados</div>
+					</div>
+					<div className='custm-grid-area grid-area-post7'>
+						<div className='custm-iPost shadow-sm'>Semaforo</div>
+					</div>
+
+					{/* <div className='custm-iPost d-flex justify-content-center align-items-center flex-column shadow'>
 						<h3>
 							¡Hola, <span style={{ textTransform: 'capitalize' }}>{firstName}</span>!
 						</h3>
@@ -19,8 +41,7 @@ const InicioPage = () => {
 						</p>
 					</div>
 					<div className='custm-iPost d-flex justify-content-center align-items-center flex-column shadow'></div>
-					<div className='custm-iPost d-flex justify-content-center align-items-center flex-column shadow'></div>
-					<div className='custm-iPost d-flex justify-content-center align-items-center flex-column shadow'></div>
+					<div className='custm-iPost d-flex justify-content-center align-items-center flex-column shadow'></div> */}
 				</div>
 			</div>
 		</>
