2a171ce0517844b15fb92a939f1758427daafb49
Ivan <ivan.santana@ulfix.com>
2022-02-11 10:01:14 -0600

    InicioPAge - Listo

--- frontend/src/components/inicioPage/InicioPage.tsx
+++ frontend/src/components/inicioPage/InicioPage.tsx
@@ -20,22 +20,73 @@ const InicioPage = () => {
 						</div>
 					</div>
 					<div className='custm-grid-area grid-area-post2'>
-						<div className='custm-iPost2 custm-postShadow'>Tareas pendientes</div>
+						<div className='custm-iPost2 custm-postShadow d-flex flex-column align-items-center'>
+							<div className='custm-PostTittle'>TAREAS PENDIENTES</div>
+							<div className='custm-postBody'>
+								<label className='textColorLight fs-4'>No tienes nada pendiente.</label>
+							</div>
+						</div>
 					</div>
 					<div className='custm-grid-area grid-area-post3'>
-						<div className='custm-iPost custm-postShadow'>Avisos</div>
+						<div className='custm-iPost custm-postShadow d-flex flex-column align-items-center'>
+							<div className='custm-PostTittle'>AVISOS</div>
+							<div className='custm-postBody'>
+								<label className='textColorLight fs-4 text-center'>
+									No hay ningún aviso en este momento.
+								</label>
+							</div>
+						</div>
 					</div>
 					<div className='custm-grid-area grid-area-post4'>
-						<div className='custm-iPost custm-postShadow'>Proximas ausencias</div>
+						<div className='custm-iPost custm-postShadow d-flex flex-column align-items-center'>
+							<div className='custm-PostTittle'>PROXIMAS AUSENCIAS</div>
+							<div className='custm-postBody'>
+								<label className='textColorLight fs-4 text-center'>
+									No hay ausencias próximamente.
+								</label>
+							</div>
+						</div>
 					</div>
 					<div className='custm-grid-area grid-area-post5'>
-						<div className='custm-iPost custm-postShadow'>Proximos eventos</div>
+						<div className='custm-iPost custm-postShadow d-flex flex-column align-items-center'>
+							<div className='custm-PostTittle'>PROXIMOS EVENTOS</div>
+							<div className='custm-postBody'>
+								<label className='textColorLight fs-4 text-center'>
+									No hay eventos próximos.
+								</label>
+							</div>
+						</div>
 					</div>
 					<div className='custm-grid-area grid-area-post6'>
-						<div className='custm-iPost custm-postShadow'>Proximos feriados</div>
+						<div className='custm-iPost custm-postShadow d-flex flex-column align-items-center'>
+							<div className='custm-PostTittle'>PROXIMOS FERIADOS</div>
+							<div className='custm-postBody'>
+								<label className='textColorLight fs-4 text-center'>
+									No hay vacaciones próximas.
+								</label>
+							</div>
+						</div>
 					</div>
 					<div className='custm-grid-area grid-area-post7'>
-						<div className='custm-iPost custm-postShadow'>Semaforo</div>
+						<div className='custm-iPost custm-postShadow d-flex flex-column align-items-center'>
+							<div className='custm-PostTittle'>SEMAFORO</div>
+							<div className='custm-postBody'>
+								<label className='textColorLight fs-1 text-center'>
+									<span style={{ color: '#76FF73', padding: '0px 3px' }}>
+										<i className='bi bi-emoji-laughing-fill' />
+									</span>
+									<span style={{ color: '#75ff7359', padding: '0px 3px' }}>
+										<i className='bi bi-emoji-smile-fill' />
+									</span>
+									<span style={{ color: '#FFB34259', padding: '0px 3px' }}>
+										<i className='bi bi-emoji-neutral-fill' />
+									</span>
+									<span style={{ color: '#FF5C5C59', padding: '0px 3px' }}>
+										<i className='bi bi-emoji-frown-fill' />
+									</span>
+								</label>
+							</div>
+						</div>
 					</div>
 
 					{/* <div className='custm-iPost d-flex justify-content-center align-items-center flex-column shadow'>
