fe0af3ff1a73f94cbab28e83721d451fd69b0366
Ivan <ivan.santana@ulfix.com>
2022-02-11 03:11:25 -0600

    creando post 1 - pendiente

--- frontend/src/components/inicioPage/InicioPage.tsx
+++ frontend/src/components/inicioPage/InicioPage.tsx
@@ -10,25 +10,32 @@ const InicioPage = () => {
 			<div className='custm-inicioContainer'>
 				<div className=' custm-ContainerListGrid'>
 					<div className='custm-grid-area grid-area-post1'>
-						<div className='custm-iPost1 shadow-sm'>Bien venido</div>
+						<div className='custm-iPost1 custm-postShadow d-flex flex-column justify-content-center align-items-center'>
+							<div className='fs-1 fw-bold'>¡Hola {firstName}!</div>
+							<div className='fs-3' style={{ height: '28px' }}>
+								Bienvenido de vuelta
+							</div>
+							<div className='fs-3 '>esto es lo nuevo por aqui...</div>
+							<div className='custm-underLine'></div>
+						</div>
 					</div>
 					<div className='custm-grid-area grid-area-post2'>
-						<div className='custm-iPost2 shadow-sm'>Tareas pendientes</div>
+						<div className='custm-iPost2 custm-postShadow'>Tareas pendientes</div>
 					</div>
 					<div className='custm-grid-area grid-area-post3'>
-						<div className='custm-iPost shadow-sm'>Avisos</div>
+						<div className='custm-iPost custm-postShadow'>Avisos</div>
 					</div>
 					<div className='custm-grid-area grid-area-post4'>
-						<div className='custm-iPost shadow-sm'>Proximas ausencias</div>
+						<div className='custm-iPost custm-postShadow'>Proximas ausencias</div>
 					</div>
 					<div className='custm-grid-area grid-area-post5'>
-						<div className='custm-iPost shadow-sm'>Proximos eventos</div>
+						<div className='custm-iPost custm-postShadow'>Proximos eventos</div>
 					</div>
 					<div className='custm-grid-area grid-area-post6'>
-						<div className='custm-iPost shadow-sm'>Proximos feriados</div>
+						<div className='custm-iPost custm-postShadow'>Proximos feriados</div>
 					</div>
 					<div className='custm-grid-area grid-area-post7'>
-						<div className='custm-iPost shadow-sm'>Semaforo</div>
+						<div className='custm-iPost custm-postShadow'>Semaforo</div>
 					</div>
 
 					{/* <div className='custm-iPost d-flex justify-content-center align-items-center flex-column shadow'>
