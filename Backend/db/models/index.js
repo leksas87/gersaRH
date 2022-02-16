3b07fcc19f3636847676d6813061dc48b619092e
*blJlEyaGo2 <ruben.martinez@ulfix.com>
2022-02-16 14:36:13 -0600

    creacion del modelo logbook, en este se guardaran los registros de check

--- Backend/db/models/index.js
+++ Backend/db/models/index.js
@@ -1,11 +1,14 @@
 const { User, UserSchema } = require('./user.model');
 const { Employee, EmployeeSchema } = require('./employee.model');
+const { Logbook,LogbookSchema}=require('./logbook.model');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Employee.init(EmployeeSchema, Employee.config(sequelize));
+  Logbook.init(LogbookSchema,Logbook.config(sequelize));
 
   Employee.associate(sequelize.models);
+  Logbook.associate(sequelize.models);
 }
 
 module.exports = setupModels;
