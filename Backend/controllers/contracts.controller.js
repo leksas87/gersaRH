72ce9acbb791128bafbecc84a03682c546a476cf
Miguel Angel Julian Castellanos <miguel.julian@ulfix.com>
2022-02-18 10:58:02 -0600

    Se agrego el endpoint Put y Patch

--- Backend/controllers/contracts.controller.js
+++ Backend/controllers/contracts.controller.js
@@ -6,10 +6,61 @@ const authorize = require('middleware/authorize')
 const contractService = require('../services/contract.service');
 
 // routes
-router.post('/register', authorize() ,registerSchema, register);
+router.post('/', authorize() ,registerSchema, register);
+router.patch('/:id', authorize(), updateSchema, update);
+router.put('/:id', authorize(), updateSchemaPut, update);
 
 module.exports = router;
 
+
+
+function updateSchemaPut(req, res, next) {
+    const schema = Joi.object({
+        puesto: Joi.string().required(),
+        fechaDeInicio: Joi.date().required(),
+        fechaDeFinalizacion: Joi.date().required(),
+        horasLaborales: Joi.number().integer().required(),
+        unidadLaborales: Joi.string().required(),
+        lunes: Joi.boolean().required(),
+        martes: Joi.boolean().required(),
+        miercoles: Joi.boolean().required(),
+        jueves: Joi.boolean().required(),
+        viernes: Joi.boolean().required(),
+        sabado: Joi.boolean().required(),
+        domingo: Joi.boolean().required(),
+        tipoSalario: Joi.string().required(),
+        cantidadSalario: Joi.number().integer().required(),
+        isContractActivide: Joi.boolean().required()
+    });
+    validateRequest(req, next, schema);
+}
+
+function update(req, res, next) {
+    contractService.update(req.params.id, req.body)
+        .then(contract => res.json({data:contract ,message:'Succesful',ok:true}))
+        .catch(next);
+}
+
+function updateSchema(req, res, next) {
+    const schema = Joi.object({
+        puesto: Joi.string(),
+        fechaDeInicio: Joi.date(),
+        fechaDeFinalizacion: Joi.date(),
+        horasLaborales: Joi.number().integer(),
+        unidadLaborales: Joi.string(),
+        lunes: Joi.boolean(),
+        martes: Joi.boolean(),
+        miercoles: Joi.boolean(),
+        jueves: Joi.boolean(),
+        viernes: Joi.boolean(),
+        sabado: Joi.boolean(),
+        domingo: Joi.boolean(),
+        tipoSalario: Joi.string(),
+        cantidadSalario: Joi.number().integer(),
+        isContractActivide: Joi.boolean()
+    });
+    validateRequest(req, next, schema);
+}
 
 function registerSchema(req, res, next) {
     const schema = Joi.object({
