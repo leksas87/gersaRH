051854be35b0578c7df07d0765df5a9529634d88
Miguel Angel Julian Castellanos <miguel.julian@ulfix.com>
2022-02-21 15:40:37 -0600

    Se Agrego el campo tipo de contrato a la tabla contrato

--- Backend/db/models/contract.model.js
+++ Backend/db/models/contract.model.js
@@ -1,44 +1,44 @@
-
 const { Model, DataTypes, Sequelize } = require('sequelize');
 
-const {USER_TABLE} = require('./user.model');
+  const {USER_TABLE} = require('./user.model');
 
-const CONTRACT_TABLE = 'Contracts';
+  const CONTRACT_TABLE = 'Contracts';
 
-const ContractSchema = {
-  id: { allowNull: false,autoIncrement: true,primaryKey: true,type: DataTypes.INTEGER},
-  userId: { type: DataTypes.INTEGER,allowNull:true,references:{model:USER_TABLE,key:'id'},onUpdate:'CASCADE',onDelete:'SET NULL'},
-  puesto: { type: DataTypes.STRING, allowNull: true, defaultValue:'' },
-  fechaDeInicio: { type:DataTypes.DATE,allowNull:true, defaultValue: DataTypes.NOW},
-  fechaDeFinalizacion: { type:DataTypes.DATE,allowNull:true, defaultValue: DataTypes.NOW},
-  horasLaborales : {type:DataTypes.INTEGER,allowNull:true,defaultValue:0},
-  unidadLaborales: { type: DataTypes.STRING, allowNull: true, defaultValue:'' },
-  lunes: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
-  martes: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
-  miercoles: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
-  jueves: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
-  viernes: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
-  sabado: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
-  domingo: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
-  tipoSalario: { type: DataTypes.STRING, allowNull: true, defaultValue:'' },
-  cantidadSalario : {type:DataTypes.INTEGER,allowNull:true,defaultValue:0},
-  isContractActivide: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:true }
-}
-
-class Contract extends Model {
-  static associate(models) {
-    this.belongsTo(models.User,{as:'user'});
+  const ContractSchema = {
+    id: { allowNull: false,autoIncrement: true,primaryKey: true,type: DataTypes.INTEGER},
+    userId: { type: DataTypes.INTEGER,allowNull:true,references:{model:USER_TABLE,key:'id'},onUpdate:'CASCADE',onDelete:'SET NULL'},
+    tipoDeContrato: { type: DataTypes.STRING, allowNull: true, defaultValue:'' },
+    puesto: { type: DataTypes.STRING, allowNull: true, defaultValue:'' },
+    fechaDeInicio: { type:DataTypes.DATE,allowNull:true, defaultValue: DataTypes.NOW},
+    fechaDeFinalizacion: { type:DataTypes.DATE,allowNull:true, defaultValue: null},
+    horasLaborales : {type:DataTypes.INTEGER,allowNull:true,defaultValue:0},
+    unidadLaborales: { type: DataTypes.STRING, allowNull: true, defaultValue:'' },
+    lunes: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
+    martes: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
+    miercoles: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
+    jueves: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
+    viernes: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
+    sabado: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
+    domingo: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:false },
+    tipoSalario: { type: DataTypes.STRING, allowNull: true, defaultValue:'' },
+    cantidadSalario : {type:DataTypes.INTEGER,allowNull:true,defaultValue:0},
+    isContractActivide: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue:true }
   }
 
-  static config(sequelize) {
-    return {
-      sequelize,
-      tableName: CONTRACT_TABLE,
-      modelName: 'Contract',
-      timestamps: false
+  class Contract extends Model {
+    static associate(models) {
+      this.belongsTo(models.User,{as:'user'});
+    }
+
+    static config(sequelize) {
+      return {
+        sequelize,
+        tableName: CONTRACT_TABLE,
+        modelName: 'Contract',
+        timestamps: false
+      }
     }
   }
-}
 
 
-module.exports = { CONTRACT_TABLE, ContractSchema, Contract }
+  module.exports = { CONTRACT_TABLE, ContractSchema, Contract }
