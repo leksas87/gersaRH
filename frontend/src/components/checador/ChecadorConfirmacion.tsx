2f3d55b7c3b711db0b0e452ccbdbaf347e186027
Ivan <ivan.santana@ulfix.com>
2022-02-23 08:44:52 -0600

    ajuste de justificacion de textos en ChecadorConfirmacion - Listo

--- frontend/src/components/checador/ChecadorConfirmacion.tsx
+++ frontend/src/components/checador/ChecadorConfirmacion.tsx
@@ -12,18 +12,20 @@ const ChecadorConfirmacion = () => {
 					/>
 				</div>
 				<div className='d-flex flex-column align-items-center lh-sm'>
-					<div className='fs-2 fw-bold textColorSecondary'>
+					<div className='fs-2 fw-bold textColorSecondary text-center'>
 						Confírmanos que eres tú
 					</div>
 				</div>
 				<div className='d-flex flex-column align-items-center mt-5'>
-					<div className='fs-2  textColorSecondary'>Ivan Ojendis Santana</div>
+					<div className='fs-2  textColorSecondary text-center'>
+						Ivan Ojendis Santana
+					</div>
 					<div className='fs-2  textColorSecondary'>4052</div>
-					<div className='fs-5 textColorLight'>
+					<div className='fs-5 textColorLight text-center'>
 						Cordenadas: 17.5548193, -99.4877102,19z
 					</div>
 				</div>
-				<div className='mt-4 pb-5'>
+				<div className='mt-4 pb-5 d-flex flex-wrap justify-content-center'>
 					<button
 						className='btn custm-btnCheckConfirmation custm-btnCheckConfirmation1'
 						type='button'
